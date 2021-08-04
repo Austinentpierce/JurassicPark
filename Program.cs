1a335aed259a1c1d018d21c77c94cc7ff4cdd2d5
Austin Pierce <Austinentpierce@gmail.com>
2021-08-04 19:15:34 -0400

    description method made

--- Program.cs
+++ Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace JurassicPark
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Dinosaurs
     {
-      Console.WriteLine("Welcome to C#");
+        public string Name { get; set; }
+        public string DietType { get; set; }
+        public string WhenAcquired { get; set; }
+        public int Weight { get; set; }
+        public int EnclosureNumber { get; set; }
+        public string Description()
+        {
+            return Name + DietType + WhenAcquired + Weight + EnclosureNumber
+        }
     }
-  }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //static void DisplayGreeting()
+            {
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("  Welcome to Jurassic Park  ");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+
+        }
+
+        static string PromptForString(string prompt)
+
+        {
+            Console.Write(prompt);
+            var userinput = Console.ReadLine();
+
+            return userinput;
+
+        }
+    }
 }
+
+
+
